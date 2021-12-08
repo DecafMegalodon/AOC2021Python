788e5197d5658276139859c334cf476350a1a35b
DecafMegalodon <5885124+DecafMegalodon@users.noreply.github.com>
2021-12-08 14:12:24 -0500

    [D01a] Added depth comparison

--- day01a.py
+++ day01a.py
@@ -5,7 +5,14 @@ def read_input(filename="./inputs/day01.txt"):
     file = open(filename, "r")
     input = file.readlines()
     file.close()
-    input = [i.strip() for i in input]
+    input = [int(i) for i in input]
     return input
 
-read_input()
+def count_deeper(input):
+    current_depth = float('inf')  # set our initial depth to infinity so the first measurement is always less
+    deeper_count = 0
+    for new_depth in input:
+        if new_depth > current_depth:
+            deeper_count += 1
+        current_depth = new_depth
+    return deeper_count
--- tests/test_day01a.py
+++ tests/test_day01a.py
@@ -2,6 +2,14 @@ import unittest
 import day01a
 
 class TestYeah(unittest.TestCase):
+    def setUp(self):
+        self.input = input = day01a.read_input("./tests/test_inputs/day01input.txt")
+
     def test_input_read(self):
-        input = day01a.read_input("./tests/test_inputs/day01input.txt")
-        self.assertEqual(len(input), 10)
+        self.assertEqual(len(self.input), 10)
+
+    def test_input_format(self):
+        self.assertEqual(sum(self.input), 2256)
+        
+    def test_depth_comparison(self):
+        self.assertEqual(day01a.count_deeper(self.input), 7)
