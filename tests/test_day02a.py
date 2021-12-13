364d570f5da529b1f94501f57c66c2345e633abd
DecafMegalodon <5885124+DecafMegalodon@users.noreply.github.com>
2021-12-13 11:26:08 -0500

    [D02a] Complete!

--- tests/test_day02a.py
+++ tests/test_day02a.py
@@ -10,3 +10,6 @@ class TestDay02a(unittest.TestCase):
         
     def test_submarine_simulator(self):
         self.assertEqual(day02a.simulate_submarine(self.input), 150)
+        
+    def test_full_solution(self):
+        self.assertEqual(day02a.simulate_submarine(day02a.read_input()), 1670340)
