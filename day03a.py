cd4d8493ddf11896fc079791e0ece6d0cd834833
DecafMegalodon <5885124+DecafMegalodon@users.noreply.github.com>
2021-12-20 12:33:36 -0500

    [D03a] Add basic input

--- day03a.py
+++ day03a.py
@@ -1,6 +1,10 @@
 #DecafMegalodon
 #Advent of code day 03a submission
+#Our goal today is find the most common bit in each column of the input,
+#As well as the complement of the most common bits
 
 def read_input(filename="./inputs/day01.txt"):
     file = open(filename, "r")
+    input = file.readlines()
     file.close()
+    return input
--- tests/test_day03a.py
+++ tests/test_day03a.py
@@ -2,4 +2,9 @@ import unittest
 import day03a
 
 class TestDay03a(unittest.TestCase):
-    pass
+    
+    def setUp(self):
+        self.input = day03a.read_input("./tests/test_inputs/day03input.txt")
+
+    def test_input(self):
+        self.assertEqual(len(self.input), 12)  # There should be 12 lines of test input
