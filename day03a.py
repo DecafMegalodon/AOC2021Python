61a15796e2c1a03e1864bafe9d3393141449f488
DecafMegalodon <5885124+DecafMegalodon@users.noreply.github.com>
2021-12-20 12:15:07 -0500

    [D03a] Let's get started

---
+++ day03a.py
@@ -0,0 +1,6 @@
+#DecafMegalodon
+#Advent of code day 03a submission
+
+def read_input(filename="./inputs/day01.txt"):
+    file = open(filename, "r")
+    file.close()
---
+++ tests/test_day03a.py
@@ -0,0 +1,5 @@
+import unittest
+import day03a
+
+class TestDay03a(unittest.TestCase):
+    pass
